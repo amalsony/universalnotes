952078a5c162d275cf2992e1deb8cf23448d591f
Amal Sony <amalsony5@gmail.com>
2023-12-05 00:47:50 -0500

    finished nextjs setup

---
+++ next-app/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-72 w-48 bg-gray-100">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        type="button"
+      >
+        test
+      </button>
+    </div>
+  );
+}
